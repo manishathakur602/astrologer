817ee579ff3b91dcc00808bf74b9139e591ca8cc
Manish <139942794+Manish6906@users.noreply.github.com>
2025-04-14 16:33:24 +0530

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <header>
+           <div className='bg-[#FFC003]'>
+           <p className='text-center'>100% Clients Satisfaction</p>
+                    <div>
+                            <div>
+                            
+                            </div>
+                    </div>
+           </div>
+        </header>
+    </>
+  )
+}
+
+export default Navbar
